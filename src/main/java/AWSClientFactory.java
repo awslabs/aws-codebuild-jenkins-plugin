1fbb59528a3a3cd18c868b369a28727307417818
篠原 祐貴 <shinohara.yuki@mm-shinohara01.local>
2017-01-17 16:24:43 +0900

    delete blank line

--- src/main/java/AWSClientFactory.java
+++ src/main/java/AWSClientFactory.java
@@ -62,9 +62,6 @@ public class AWSClientFactory {
         if(Validation.parseInt(this.proxyPort) != null) {
             clientConfig.setProxyPort(Validation.parseInt(proxyPort));
         }
-
-
-
     }
 
     public AWSCodeBuildClient getCodeBuildClient() throws InvalidInputException {
